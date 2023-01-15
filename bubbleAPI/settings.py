0e2aed9ade3c0be18b71358be8cf9fcb733e373a
SylvainJunca <sylvain.junca@gmail.com>
2023-01-14 21:59:04 -0500

    connects mysql db

--- bubbleAPI/settings.py
+++ bubbleAPI/settings.py
@@ -78,8 +78,20 @@ WSGI_APPLICATION = 'bubbleAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
+        'NAME': os.environ.get('DB_NAME', 'bubble'),
+        'USER': os.environ.get('DB_USER', 'bubble'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'bubble'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        'TEST': {
+            'NAME': 'test_bubble',
+            'MIGRATE': False
+
+        }
     }
 }
 

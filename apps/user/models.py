0cb03d5b84bfcd8f5120f6a0a25e888a840ed009
SylvainJunca <sylvain.junca@gmail.com>
2023-10-27 11:16:42 -0400

    custom user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -2,10 +2,10 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from apps.shared.models import UUIDModel, TimestampModel
-rom django.db.models import UniqueConstraint
+from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 
-from .managers import CustomUserManager
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin, UUIDModel, TimestampModel):
@@ -13,18 +13,20 @@ class User(AbstractBaseUser, PermissionsMixin, UUIDModel, TimestampModel):
     username = models.CharField(
         'username',
         max_length=32,
-        validators=[MinLengthValidator(3), MaxLengthValidator(32)])
+        validators=[MinLengthValidator(3), MaxLengthValidator(32)],
+        null=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     def __str__(self):
         return self.email
-    
+
     class Meta:
         constraints = [
             UniqueConstraint(
